8194cd9acc5a66fae79783e9ac0d101565b5a82d
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-24 18:07:20 +0100

    Adding to Cart

--- app.py
+++ app.py
@@ -10,3 +10,13 @@ driver.fullscreen_window()
 # //a[text()="Shop"] Xpath
 # a[href*="shop"] CSS selector
 driver.find_element(By.XPATH, value='//a[text()="Shop"]').click()
+products = driver.find_elements(by='xpath', value='//div[@class="card h-100"]')
+for product in products:
+# //div[@class="card h-100"]/div/h4/a for X path
+    productName = product.find_element(By.XPATH, value='./div/h4/a').text
+    # print(productName)
+    price = product.find_element(By.XPATH, value='./div/h5').text
+    # print(price)
+    content = product.find_element(By.XPATH, value='./div/p').text
+    # print(content)
+    
